88a52139e8f2240a3e4651a75c79c420cbe7ce55
Erik Michael Purins <erik.purins@imgtec.com>
2015-04-16 09:11:15 -0700

    Backport 5.0.0 vsphere. Add checksum.

--- vmwarevsphereclient/tools/chocolateyInstall.ps1
+++ vmwarevsphereclient/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
-﻿$packageName = 'vmwarevsphereclient' 
-$installerType = 'exe' 
-$url = 'http://vsphereclient.vmware.com/vsphereclient/1/9/9/3/0/7/2/VMware-viclient-all-5.5.0-1993072.exe'
-$url64 = $url 
-$silentArgs = '/s /w /v" /qb"' 
-$validExitCodes = @(0) 
+﻿$packageName = 'vmwarevsphereclient'
+$installerType = 'exe'
+$url = 'http://vsphereclient.vmware.com/vsphereclient/6/2/3/3/7/3/VMware-viclient-all-5.0.0-623373.exe'
+$checksum = '057b7b49942d4d35d6cb41f69e110c577329240a'
+$checksumType = 'sha1'
+$silentArgs = '/s /w /v" /qb"'
+$validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType
